02706865505fa02c1abecf39e3ccc651bf353389
heibkjocker <cristian.quinterom94@hotmail.com>
2023-09-20 17:20:50 -0500

    Se genera funcionalidad para iterar en todas las paginas y sacar los productos

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://comercioelectrico.com/index.php/page/1/?s&post_type=product"
 
+def conservar_numeros(lista):
+    lista_de_numeros = [c for c in lista if c.isnumeric()]
+    return lista_de_numeros
 def get_productos_elcomercioElectrico():
-    response = requests.get(URL)
+    response = requests.get("https://comercioelectrico.com/index.php/page/1/?s&post_type=product")
     soup = BeautifulSoup(response.content, "html.parser")
-    productos = soup.find_all('h2', class_="woocommerce-loop-product__title")
-    precios = soup.find_all('bdi')
-    paginas = soup.find_all('nav', class_="woocommerce-pagination")
+    paginas = soup.find_all('a', class_="page-numbers")
+    lista_paginas = []
+    for p in paginas:
+        lista_paginas.append(p.get_text(strip=True))
+    solo_numeros = conservar_numeros(lista_paginas)
+    ultima_pagina = solo_numeros[-1]
     lista = []
     lista_precios = []
-    lista_paginas = []
-    for item in productos:
-        lista.append(item.get_text(strip=True))
-    for i in precios:
-        lista_precios.append(i.get_text(strip=True))
+
+    for i in range(0, int(ultima_pagina)):
+        URL = f"https://comercioelectrico.com/index.php/page/{i}/?s&post_type=product"
+        print(URL)
+        response = requests.get(URL)
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        productos = soup.find_all('h2', class_="woocommerce-loop-product__title")
+        precios = soup.find_all('bdi')
+
+
+        for item in productos:
+            lista.append(item.get_text(strip=True))
+        for p in precios:
+            lista_precios.append(p.get_text(strip=True))
+
     return lista, lista_precios
 
 if __name__ == "__main__":
-    print(get_productos_elcomercioElectrico())
+    etiquetas, precios = get_productos_elcomercioElectrico()
+    print(etiquetas)
+    print(precios)
+
